5ea592ccf99979cec1b72320c03fec18a3f18024
Michael King <michaelj.king@btinternet.com>
2022-06-12 18:19:31 +0100

    Regions

--- Program.fs
+++ Program.fs
@@ -3,14 +3,16 @@
         X: int
         Y: int
         Val: int option
+        RegionIndex: int
     }
 
 module Square = 
-    let create x y v = 
+    let create x y v region = 
         {
             X = x
             Y = y
             Val = v
+            RegionIndex = region
         }
 
     let print s = 
@@ -18,15 +20,34 @@ module Square =
         | None -> sprintf "_"
         | Some i -> sprintf "%d" i
 
+    let printRegion s = sprintf "%d" s.RegionIndex
+
+    let calculateRegion x y = 
+        let regions = [
+            [ 2; 2; 2; 2; 10; 10; 17; 17; 17; 17 ]
+            [ 2; 2; 2; 10; 10; 17; 17; 23; 23; 17 ]
+            [ 2; 2; 7; 10; 9; 16; 17; 23; 23; 17 ]
+            [ 1; 2; 7; 9; 9; 9; 20; 22; 22; 22 ]
+            [ 1; 5; 4; 9; 13; 15; 20; 20; 24; 24 ]
+            [ 1; 4; 4; 8; 8; 15; 19; 14; 14; 24 ]
+            [ 1; 1; 4; 6; 8; 14; 14; 14; 24; 24 ]
+            [ 1; 1; 6; 6; 12; 12; 14; 18; 24; 24 ]
+            [ 1; 1; 3; 3; 3; 11; 18; 18; 11; 11 ]
+            [ 1; 3; 3; 3; 11; 11; 11; 11; 11; 11 ]
+        ]
+
+        regions[y][x]
+
+
 let mutable grid: Square list = []
 
 module Grid = 
-    let appendEmptySquare grid x y = 
-        let s = Square.create x y None
+    let appendEmptySquare grid x y region = 
+        let s = Square.create x y None region
         s::grid
 
-    let appendSquare grid x y v = 
-        let s = Square.create x y (Some v)
+    let appendSquare grid x y v region = 
+        let s = Square.create x y (Some v) region
         s::grid
 
     let printRow (grid: Square list) i = 
@@ -44,6 +65,21 @@ module Grid =
             (Square.print row[8])
             (Square.print row[9])
 
+    let printRegionRow (grid: Square list) i = 
+        let row = grid |> List.where (fun s -> s.Y = i) |> List.sortBy (fun s -> s.X) 
+
+        printfn "%s | %s | %s | %s | %s | %s | %s | %s | %s | %s" 
+            (Square.printRegion row[0])
+            (Square.printRegion row[1])
+            (Square.printRegion row[2])
+            (Square.printRegion row[3])
+            (Square.printRegion row[4])
+            (Square.printRegion row[5])
+            (Square.printRegion row[6])
+            (Square.printRegion row[7])
+            (Square.printRegion row[8])
+            (Square.printRegion row[9])
+
     let print (grid: Square list) = 
         printRow grid 9
         printRow grid 8
@@ -56,42 +92,41 @@ module Grid =
         printRow grid 1
         printRow grid 0
 
+        
+    let printRegion (grid: Square list) = 
+        printRegionRow grid 9
+        printRegionRow grid 8
+        printRegionRow grid 7
+        printRegionRow grid 6
+        printRegionRow grid 5
+        printRegionRow grid 4
+        printRegionRow grid 3
+        printRegionRow grid 2
+        printRegionRow grid 1
+        printRegionRow grid 0
+
 for x in 0..9 do
     for y in 0..9 do 
         grid <-
             match x, y with 
-            | 0, 5 -> Grid.appendSquare grid x y 6
-            | 1, 9 -> Grid.appendSquare grid x y 3
-            | 1, 2 -> Grid.appendSquare grid x y 2
-            | 2, 5 -> Grid.appendSquare grid x y 1
-            | 3, 6 -> Grid.appendSquare grid x y 1
-            | 3, 8 -> Grid.appendSquare grid x y 4
-            | 4, 0 -> Grid.appendSquare grid x y 5
-            | 5, 9 -> Grid.appendSquare grid x y 7
-            | 6, 1 -> Grid.appendSquare grid x y 6
-            | 6, 3 -> Grid.appendSquare grid x y 2
-            | 7, 4 -> Grid.appendSquare grid x y 3
-            | 8, 0 -> Grid.appendSquare grid x y 2
-            | 8, 7 -> Grid.appendSquare grid x y 2
-            | 9, 4 -> Grid.appendSquare grid x y 6
-            | _ -> Grid.appendEmptySquare grid x y 
-        //if x = 0 && y = 5 then grid <- Grid.appendSquare grid x y 6
-        //if x = 1 && y = 9 then grid <- Grid.appendSquare grid x y 3
-        //if x = 1 && y = 2 then grid <- Grid.appendSquare grid x y 2
-        //if x = 2 && y = 5 then grid <- Grid.appendSquare grid x y 1
-        //if x = 3 && y = 6 then grid <- Grid.appendSquare grid x y 1
-        //if x = 3 && y = 8 then grid <- Grid.appendSquare grid x y 4
-        //if x = 4 && y = 0 then grid <- Grid.appendSquare grid x y 5
-        //if x = 5 && y = 9 then grid <- Grid.appendSquare grid x y 7
-        //if x = 6 && y = 1 then grid <- Grid.appendSquare grid x y 6
-        //if x = 6 && y = 3 then grid <- Grid.appendSquare grid x y 2
-        //if x = 7 && y = 4 then grid <- Grid.appendSquare grid x y 3
-        //if x = 8 && y = 0 then grid <- Grid.appendSquare grid x y 2
-        //if x = 8 && y = 7 then grid <- Grid.appendSquare grid x y 2
-        //if x = 9 && y = 4 then grid <- Grid.appendSquare grid x y 6
-        //else grid <- Grid.appendEmptySquare grid x y 
+            | 0, 5 -> Grid.appendSquare grid x y 6 (Square.calculateRegion x y) 
+            | 1, 9 -> Grid.appendSquare grid x y 3 (Square.calculateRegion x y)
+            | 1, 2 -> Grid.appendSquare grid x y 2 (Square.calculateRegion x y)
+            | 2, 5 -> Grid.appendSquare grid x y 1 (Square.calculateRegion x y)
+            | 3, 6 -> Grid.appendSquare grid x y 1 (Square.calculateRegion x y)
+            | 3, 8 -> Grid.appendSquare grid x y 4 (Square.calculateRegion x y)
+            | 4, 0 -> Grid.appendSquare grid x y 5 (Square.calculateRegion x y)
+            | 5, 9 -> Grid.appendSquare grid x y 7 (Square.calculateRegion x y)
+            | 6, 1 -> Grid.appendSquare grid x y 6 (Square.calculateRegion x y)
+            | 6, 3 -> Grid.appendSquare grid x y 2 (Square.calculateRegion x y)
+            | 7, 4 -> Grid.appendSquare grid x y 3 (Square.calculateRegion x y)
+            | 8, 0 -> Grid.appendSquare grid x y 2 (Square.calculateRegion x y)
+            | 8, 7 -> Grid.appendSquare grid x y 2 (Square.calculateRegion x y)
+            | 9, 4 -> Grid.appendSquare grid x y 6 (Square.calculateRegion x y)
+            | _ -> Grid.appendEmptySquare grid x y (Square.calculateRegion x y) 
 
 Grid.print grid
+Grid.printRegion grid
 
 
         
